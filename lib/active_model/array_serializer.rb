a60d326f3a176d2c514ac9987970fb08e310912c
Charlie White <charlie@anchor.io>
2013-09-14 13:04:20 -0700

    testing

--- lib/active_model/array_serializer.rb
+++ lib/active_model/array_serializer.rb
@@ -52,13 +52,13 @@ module ActiveModel
         end
         serializer ||= DefaultSerializer
 
-        serializable = serializer.new(item, options.merge(root: nil))
+        serializable = serializer.new(item, options)
 
-        if serializable.respond_to?(:serializable_hash)
-          serializable.serializable_hash
-        else
+        # if serializable.respond_to?(:serializable_hash)
+        #   serializable.serializable_hash
+        # else
           serializable.as_json
-        end
+        # end
       end
     end
   end
